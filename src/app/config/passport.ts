fec9cf39cf9cb3fb152282c1dfb511e3c8c543f2
Shanto93 <shantoislam7363@gmail.com>
2025-07-16 15:16:28 +0600

    Create passport.ts file for implement google strategy

---
+++ src/app/config/passport.ts
@@ -0,0 +1,53 @@
+import passport from "passport";
+import {
+  Strategy as GoogleStrategy,
+  type Profile,
+  type VerifyCallback,
+} from "passport-google-oauth20";
+import { EnvConfig } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: EnvConfig.GOOGLE_CLIENT_ID,
+      clientSecret: EnvConfig.GOOGLE_CLIENT_SECRET,
+      callbackURL: EnvConfig.GOOGLE_CALLBACK_URL,
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const email = profile.emails?.[0].value;
+        if (!email) {
+          return done(null, false, { message: "Email not found" });
+        }
+
+        let user = await User.findOne({ email });
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            isVerified: true,
+            role: Role.USER,
+            auths: [
+              {
+                provider: "google",
+                providerId: profile.id,
+              },
+            ],
+          });
+        }
+        return done(null, user, { message: "User created successfully" });
+      } catch (error) {
+        console.log("Google Strategy error: ", error);
+        return done(error);
+      }
+    }
+  )
+);
